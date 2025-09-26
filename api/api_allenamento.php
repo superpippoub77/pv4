1ca2c3dcf9c3026d852b5478a0df18566fcb27da
Filippo Morano <fmorano@deltain.net>
2025-09-26 16:11:52 +0200

    fix volleyball_exercise_library

--- api/api_allenamento.php
+++ api/api_allenamento.php
@@ -14,7 +14,7 @@ if (empty($filename) || strpos($filename, '..') !== false || pathinfo($filename,
     exit;
 }
 
-$library_dir = 'libreria/';
+$library_dir = 'volleyball_exercise_library/';
 $file_path = $library_dir . basename($filename); // basename() previene manipolazioni path
 
 // Verifica se il file esiste
