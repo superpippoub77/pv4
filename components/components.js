d9c9e9ad3f902f15938d7bead3933d2f6713596a
Filippo Morano <fmorano@deltain.net>
2025-11-14 17:28:46 +0100

    create schemaeditor.js helper.js and add component in components.js

--- components/components.js
+++ components/components.js
@@ -73,3 +73,15 @@ class TeamManagementDialog extends HTMLElement {
 }
 
 customElements.define("team-management-dialog", TeamManagementDialog);
+
+
+class WaterMark extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `    <div id="watermark">
+        VBProW4 by <a href="https://www.filippomorano.com" target="_blank" rel="noopener noreferrer">spikecode</a>
+    </div>
+        `;
+    }
+}
+
+customElements.define("water-mark", WaterMark);
